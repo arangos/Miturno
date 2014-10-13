15c80296ed149e45ba2ee8d191b1909a93f2f4a9
Santiago Vasco <svascot@gmail.com>
2014-10-13 15:51:09 +0200

    cambio en el query de las listas de las dependencias

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -101,7 +101,7 @@ class SiteController extends Controller {
 		
 		
 		
-		$sqlTurnoActual = new CSqlDataProvider ( "SELECT distinct(NombreDependencia) FROM test_turnos_pedidos" );
+		$sqlTurnoActual = new CSqlDataProvider ( "SELECT NombreDependencia FROM dependencia" );
 		$sqlTurnoActual = $sqlTurnoActual->getData ();
 	
 		for($var1 = 0; $var1< count($sqlTurnoActual);$var1++){
