7419d2df9a357b77ae6e6d0f0b7ecd8ae082dfaa
Santiago Vasco <svascot@gmail.com>
2014-10-17 00:53:14 +0200

    como dependencia por post

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -138,8 +138,7 @@ class SiteController extends Controller {
 	public function actionPedirTurno(){
 		
 		$dato = $_POST['nombreDep'];
-		echo $dato;
-		
+			
 		$connection = Yii::app ()->db;
 		
 		$an = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -152,7 +151,7 @@ class SiteController extends Controller {
 		substr($an, rand(0, $su), 1);
 		
 		$sqlUltimoTurno = new CSqlDataProvider ( "SELECT * FROM test_turnos_pedidos WHERE Turno = (
-    select max(Turno) from test_turnos_pedidos where NombreDependencia = '" . $dato ."') and NombreDependencia = ''" );
+    select max(Turno) from test_turnos_pedidos where NombreDependencia = '" . $dato ."') and NombreDependencia = ' . $dato .'" );
 		$sqlUltimoTurno = $sqlUltimoTurno->getData ();
 		
 		if ($sqlUltimoTurno != null) {
