2473a93578f384483bc3d6a594428179a0ed87b4
ARANGO CARDONA ANDRES <andresac2004@hotmail.com>
2014-10-27 22:52:11 -0500

    TDD DBTEST

---
+++ protected/tests/DbTest.php
@@ -0,0 +1,33 @@
+<?php
+Class DbTest extends CTestCase
+{
+	public function testConnection()
+	{
+$servername = "localhost";
+	$username = "root";
+	$password = "";
+	
+	
+	// Crear la coneccion
+	try {
+		//en conn el myslq(servidor,usuario,pass,db,puerto,socket)
+		$conn = new mysqli($servername, $username, $password);
+	} catch (Exception $e) {
+		print $e;
+		
+	}
+	
+	
+	// Check conection
+	if($conn->connect_error){
+		echo("no hay coneccion");
+	}else{
+		echo ("conecto");
+		$this->assertTrue(true);
+	}
+	echo ("termine validaciones");
+	
+	
+	}
+	}
+?>
--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -7,4 +7,5 @@ $config=dirname(__FILE__).'/../config/test.php';
 require_once($yiit);
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
+
 Yii::createWebApplication($config);
